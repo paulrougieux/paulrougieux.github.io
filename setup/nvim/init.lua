a431d407548706407caaafbb59f1e1b4d67778e0
Paul Rougieux <paul.rougieux@gmail.com>
2025-10-09 18:40:29 +0200

    Install neovim plugins with lazy

--- setup/nvim/init.lua
+++ setup/nvim/init.lua
@@ -1,23 +1,20 @@
 -- Prepend ~/.vim and append ~/.vim/after to runtimepath
-vim.opt.runtimepath:prepend("~/.vim")
-vim.opt.runtimepath:append("~/.vim/after")
+-- vim.opt.runtimepath:prepend("~/.vim")
+-- vim.opt.runtimepath:append("~/.vim/after")
 
 -- Set packpath equal to runtimepath
-vim.o.packpath = vim.o.runtimepath
+-- vim.o.packpath = vim.o.runtimepath
 
 -- Source your ~/.vimrc file (Vimscript)
 vim.cmd("source ~/.vimrc")
 
-require("config.lazy")
 
+require("config.lazy")
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = { "lua", "vim", "vimdoc", "query" },  -- add vimdoc here
   highlight = { enable = true },
   auto_install = true,
 }
-
-
-
 
 
--- setup/nvim/lua/config/lazy.lua
+++ setup/nvim/lua/config/lazy.lua
@@ -21,15 +21,53 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Added by Paul
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+
+    -- Colour theme
+    {
+      "nanotech/jellybeans.vim",
+      priority = 1000,
+    },
+
+    -- Slime send text to a live Read Eval Print Loop
+    {
+      "jpalardy/vim-slime",
+    },
+
+    -- Git interface
+    {
+      "tpope/vim-fugitive",
+      cmd = { "Git", "G", "Gwrite", "Gread", "Gdiffsplit" },
+    },
+    "tpope/vim-dispatch",
+
+    -- Markdown table of contents
+    {
+      "vim-voom/VOoM",
+      ft = "markdown",
+    },
+
+    -- Python linter
+    {
+      "dense-analysis/ale",
+      event = { "BufReadPre", "BufNewFile" },
+      config = function()
+        -- Add your ALE configuration here if needed
+      end,
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+})
+
+
+require("lazy").setup({
 })
