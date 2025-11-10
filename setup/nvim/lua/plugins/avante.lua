a9871cac288b329dffdec63bff84d948632ce2dd
Paul Rougieux <paul.rougieux@gmail.com>
2025-11-10 11:20:33 +0100

    Moving avante to a previous commit

--- setup/nvim/lua/plugins/avante.lua
+++ setup/nvim/lua/plugins/avante.lua
@@ -8,6 +8,7 @@ return {
         or "make",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
+    commit = "216ba4a8b2b75", -- Revert to a previous commit if needed
     ---@module 'avante'
     ---@type avante.Config
     opts = {
@@ -31,6 +32,9 @@ return {
           model = "grok-code-fast-1", -- Or "grok-4" / keep as  for speed
           temperature = 0.2,
           max_tokens = 4096,
+          -- This was turned off in November 2025 because the API reported an error 
+          -- use_response_api = false,  -- https://github.com/yetone/avante.nvim/issues/2825
+          -- this doesn't actually work turning off again
         },
       },
     },
@@ -38,14 +42,14 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      -- "nvim-mini/mini.pick", -- for file_selector provider mini.pick
       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "stevearc/dressing.nvim", -- for input provider dressing
-      "folke/snacks.nvim", -- for input provider snacks
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      -- "stevearc/dressing.nvim", -- for input provider dressing
+      -- "folke/snacks.nvim", -- for input provider snacks
+      -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      -- "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
